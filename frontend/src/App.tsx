b7bbcc8b02b8c0faee2898d3970a366954741aaa
Himangshu Mondal <mailtoherock@gmail.com>
2023-10-12 00:57:29 +0530

    added tailwind-prettier-plugin and shahcn ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import { Button } from "@/components/ui/button";
 function App() {
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-    </>
+    <div className="container">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="grid grid-cols-2 gap-4">
+        <Button>option 1</Button>
+        <Button>option 2</Button>
+        <Button>option 3</Button>
+        <Button>option 4</Button>
+      </div>
+    </div>
   );
 }
 
