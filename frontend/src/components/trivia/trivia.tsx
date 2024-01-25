43036303462375bb5f3249a11adb6afdefc982f2
Himangshu Mondal <mailtoherock@gmail.com>
2024-01-26 01:17:56 +0530

    ui: added select state styling to answer buttons

--- frontend/src/components/trivia/trivia.tsx
+++ frontend/src/components/trivia/trivia.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+
 type Answer = {
   id: number;
   value: string;
 };
+
 type TriviaProps = {
   question: string;
   answers: Answer[];
@@ -13,16 +15,20 @@ const Trivia = ({ question, answers }: TriviaProps) => {
   return (
     <div className="rounded-md border bg-white p-10 shadow-2xl">
       <h2 className="py-4 text-xl">{question}</h2>
-      <RadioGroup defaultValue="option-one" className="grid grid-cols-2 gap-4">
+      <RadioGroup className="grid grid-cols-2 gap-4">
         {answers.map((item) => (
           <div
-            className=" relative flex items-center space-x-2 rounded-md bg-gray-100 px-8 py-4 ring-2 ring-gray-300"
+            className="has-[span]:ring-indigo-600 group relative flex items-center space-x-2 rounded-md bg-gray-100 px-8 py-4 ring-2 ring-gray-300"
             key={item.id}
           >
-            <RadioGroupItem value={item.value} id={item.value} />
+            <RadioGroupItem
+              value={item.value}
+              id={item.value}
+              className="data-[state=checked]:border-indigo-600 data-[state=checked]:text-indigo-600"
+            />
             <Label
               htmlFor={item.value}
-              className="cursor-pointer overflow-hidden after:absolute after:inset-0"
+              className="group-has-[span]:text-indigo-600 cursor-pointer overflow-hidden after:absolute after:inset-0"
             >
               {item.value}
             </Label>
