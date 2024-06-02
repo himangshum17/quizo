a4fce321ff96fa0680c2737ffa725d00e4f56562
Himangshu Mondal <mailtoherock@gmail.com>
2024-06-02 23:53:12 +0530

    feat: add basic /test API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,12 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
 const PORT = process.env.PORT ?? 5000;
 
 app.listen(PORT, () => {
   console.log(`server started on port: ${PORT}`);
+});
+
+app.get('/test', (req: express.Request, res: express.Response) => {
+  res.send('its working...');
 });
