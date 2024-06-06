bc513094724016db1e22a3d542cedf685d71f151
Raju <raju.swadesh2019@gmail.com>
2024-06-06 18:51:18 +0530

    ui: add constant categoryList

--- frontend/src/pages/selectcategory/selectcategory.page.tsx
+++ frontend/src/pages/selectcategory/selectcategory.page.tsx
@@ -1,6 +1,48 @@
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { Link } from "react-router-dom";
 
+const categoryList = [
+  {
+    id: 1,
+    name: 'Fun Quizzes',
+    image: 'https://blog.woobox.com/wp-content/uploads/8-Best-Quiz-Ideas-to-Entertain-Inform-Your-Audience-v1.png',
+  },
+  {
+    id: 2,
+    name: 'Trivia Quizzes',
+    image: 'https://blog.woobox.com/wp-content/uploads/8-Best-Quiz-Ideas-to-Entertain-Inform-Your-Audience-v1.png',
+  },
+  {
+    id: 3,
+    name: 'Animal Quizzes',
+    image: 'https://blog.woobox.com/wp-content/uploads/8-Best-Quiz-Ideas-to-Entertain-Inform-Your-Audience-v1.png',
+  },
+  {
+    id: 4,
+    name: 'Education Quizzes',
+    image: 'https://blog.woobox.com/wp-content/uploads/8-Best-Quiz-Ideas-to-Entertain-Inform-Your-Audience-v1.png',
+  },
+  {
+    id: 5,
+    name: 'Car Quizzes',
+    image: 'https://blog.woobox.com/wp-content/uploads/8-Best-Quiz-Ideas-to-Entertain-Inform-Your-Audience-v1.png',
+  },
+  {
+    id: 6,
+    name: 'Beauty Quizzes',
+    image: 'https://blog.woobox.com/wp-content/uploads/8-Best-Quiz-Ideas-to-Entertain-Inform-Your-Audience-v1.png',
+  },
+  {
+    id: 7,
+    name: 'Blogging Quizzes',
+    image: 'https://blog.woobox.com/wp-content/uploads/8-Best-Quiz-Ideas-to-Entertain-Inform-Your-Audience-v1.png',
+  },
+  {
+    id: 8,
+    name: 'Business Quizzes',
+    image: 'https://blog.woobox.com/wp-content/uploads/8-Best-Quiz-Ideas-to-Entertain-Inform-Your-Audience-v1.png',
+  },
+]
 const SelectCategory = () => {
   return (
     <section className="py-20">
@@ -9,17 +51,17 @@ const SelectCategory = () => {
           Select the cateory in which you want to proceed.
         </h1>
         <div className="mt-10 grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          {[1, 2, 3, 4, 5, 6, 7, 8].map((index) => {
+          {categoryList.map((item) => {
             return (
-              <Link to={"#"} key={index}>
+              <Link to={"#"} key={item.id}>
                 <Card>
                   <CardHeader>
                     <img
-                      src="https://placehold.co/400x400/png"
+                      src={item?.image}
                       className="mb-3"
                       alt="alt text"
                     />
-                    <CardTitle>Card Title</CardTitle>
+                    <CardTitle>{item?.name}</CardTitle>
                   </CardHeader>
                 </Card>
               </Link>
