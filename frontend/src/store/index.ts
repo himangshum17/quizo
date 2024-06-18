b7f7f218be9544f96f8b30e38c669b7476899b82
RajuMondalDev <mondalraju2396@gmail.com>
2024-06-18 19:16:36 +0530

    feature: intialize redux in main.tsx file

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,22 +1,32 @@
-import {FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer, persistStore,} from 'redux-persist'
-import {configureStore} from '@reduxjs/toolkit'
-import { rootReducer } from './root.reducer';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from "redux-persist";
+import { configureStore } from "@reduxjs/toolkit";
+import { rootReducer } from "./root.reducer";
+import storage from "redux-persist/lib/storage";
 const persistConfig = {
-    key: 'root',
-    storage: localStorage,
+  key: "root",
+  storage: storage,
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-reducer: persistedReducer,
-middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-    serializableCheck:{
-        ignoredActions: [FLUSH,REHYDRATE,PAUSE,PERSIST,PURGE,REGISTER],
-    }
-}),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
 export type AppDispatch = typeof store.dispatch;
-
--- frontend/src/store/root.reducer.ts
+++ frontend/src/store/root.reducer.ts
@@ -1,5 +1,5 @@
 import { combineReducers } from "@reduxjs/toolkit";
-import { authSlice } from "./reducer/auth";
+import authSlice from "./reducer/auth";
 
 export const rootReducer = combineReducers({
   auth: authSlice,
