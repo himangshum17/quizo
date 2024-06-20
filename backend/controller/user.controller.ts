f0e374ad70d19b8b77f2bc20dd439088b914f4f6
Himangshu Mondal <mailtoherock@gmail.com>
2024-06-20 16:39:03 +0530

    feature: authentication: add basic validation and trycatch on user controller createuser

--- backend/controller/user.controller.ts
+++ backend/controller/user.controller.ts
@@ -2,28 +2,40 @@ import { Request, Response } from "express";
 import prisma from "../config/db.config";
 
 export const createUser = async (req: Request, res: Response) => {
-  const { fullname, username, email, password } = req.body;
+  try {
+    const { fullname, username, email, password } = req.body;
 
-  const isUserEmailExists = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  });
+    // validation
+    if (!fullname || !username || !email || !password) {
+      return res.json({
+        status: 400,
+        message: "Please enter all required fields.",
+      });
+    }
 
-  if (isUserEmailExists) {
-    return res.json({
-      status: 400,
-      message: "Email already exists, please use another email",
+    const isUserEmailExists = await prisma.user.findUnique({
+      where: {
+        email,
+      },
     });
+
+    if (isUserEmailExists) {
+      return res.json({
+        status: 400,
+        message: "Email already exists, please use another email",
+      });
+    }
+
+    const newUser = await prisma.user.create({
+      data: { fullname, username, email, password },
+    });
+
+    return res.json({
+      status: 200,
+      data: newUser,
+      message: "User created successfully",
+    });
+  } catch (error) {
+    return res.status(500).send();
   }
-
-  const newUser = await prisma.user.create({
-    data: { fullname, username, email, password },
-  });
-
-  return res.json({
-    status: 200,
-    data: newUser,
-    message: "User created successfully",
-  });
 };
