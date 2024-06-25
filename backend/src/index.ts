2a1192fa9aa5d52c8043ad7965bea202377551f9
Himangshu Mondal <mailtoherock@gmail.com>
2024-06-26 00:27:07 +0530

    feature: authentication: add cors on backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,19 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 import routes from "../routes/index";
 
 const app = express();
 
 const PORT = process.env.PORT ?? 5000;
 
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
 // middlewares
 app.use(express.json());
+app.use(cors(corsOptions));
 
 // routes
 app.use(routes);
