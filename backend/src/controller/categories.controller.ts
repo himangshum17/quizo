5fd5622f6febdc92ccf09cc8e441df29f88fa8bc
Himangshu Mondal <mailtoherock@gmail.com>
2024-07-09 23:50:19 +0530

    feature: create categories table in DB and add getCategories and addCategories API endpoint

--- backend/src/controller/categories.controller.ts
+++ backend/src/controller/categories.controller.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
+import prisma from "../config/db.config";
 
 export const getCategories = async (
   req: Request,
@@ -6,10 +8,69 @@ export const getCategories = async (
   next: NextFunction,
 ) => {
   try {
+    // fetching all categories from DB
+    const allCateGories = await prisma.categories.findMany();
     // sending the categories
     return res.json({
       status: 200,
+      resultObject: allCateGories,
       message: "Categories fetched successfully",
+    });
+  } catch (error) {
+    return res.status(500).send();
+  }
+};
+
+export const addCategories = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { categoryName, categoryImage } = req.body;
+
+    // validation
+    if (!categoryName || !categoryImage) {
+      const error = createHttpError(400, "Please enter all required fields.");
+      return next(error);
+    }
+
+    if (categoryName.length < 2) {
+      const error = createHttpError(
+        400,
+        "Please enter a categoryName of at least 2 characters.",
+      );
+      return next(error);
+    }
+
+    const isCategoryNameExists = await prisma.categories.findUnique({
+      where: {
+        categoryName,
+      },
+    });
+
+    if (isCategoryNameExists) {
+      const error = createHttpError(
+        400,
+        "Category name already exists, please use another Category name.",
+      );
+      return next(error);
+    }
+
+    // createting the new category in DB
+    const newCategory = await prisma.categories.create({
+      data: { categoryName, categoryImage },
+    });
+
+    // sending the categories
+    return res.json({
+      status: 200,
+      resultObject: {
+        id: newCategory.id,
+        categoryName: newCategory.categoryName,
+        categoryImage: newCategory.categoryImage,
+      },
+      message: "Category added successfully",
     });
   } catch (error) {
     return res.status(500).send();
--- backend/src/routes/categories.route.ts
+++ backend/src/routes/categories.route.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { getCategories } from "../controller/categories.controller";
+import {
+  addCategories,
+  getCategories,
+} from "../controller/categories.controller";
 
 const router = Router();
 
 // routes
 router.get("/", getCategories);
+router.post("/", addCategories);
 
 export default router;
