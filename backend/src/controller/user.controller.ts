b303e86a40aa45aa9e8c4708fdfbc6c62eddfae6
Himangshu Mondal <mailtoherock@gmail.com>
2025-01-06 18:03:55 +0530

    feature: add user api to fetch user with valid cookie

--- backend/src/controller/user.controller.ts
+++ backend/src/controller/user.controller.ts
@@ -1,5 +1,20 @@
+import prisma from "../config/db.config";
+import { NOT_FOUND, OK } from "../constants/http";
+import { appAssert } from "../utils/appAssert";
 import { catchErrors } from "../utils/catchErrors";
 
 export const getUser = catchErrors(async (req, res) => {
-  res.json({ message: "its working" });
+  const user = await prisma.user.findUnique({
+    where: { id: req.userId },
+  });
+  appAssert(user, NOT_FOUND, "User not found");
+  res.status(OK).json({
+    resultObject: {
+      id: user.id,
+      fullname: user.fullname,
+      username: user.username,
+      email: user.email,
+    },
+    message: "User fetched successfully",
+  });
 });
