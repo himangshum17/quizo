d05852d93bac2dd36f0c23f84423efb855ae2488
Himangshu Mondal <mailtoherock@gmail.com>
2025-01-06 18:07:34 +0530

    refactor: remove redux-persist

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,8 +5,7 @@ import "./index.css";
 import { router } from "@/routes";
 import { Toaster } from "@/components/ui/toaster";
 import { Provider } from "react-redux";
-import { persistor, store } from "./store";
-import { PersistGate } from "redux-persist/integration/react";
+import { store } from "./store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
@@ -14,10 +13,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <RouterProvider router={router} />
-          <Toaster />
-        </PersistGate>
+        <RouterProvider router={router} />
+        <Toaster />
       </Provider>
     </QueryClientProvider>
   </React.StrictMode>,
